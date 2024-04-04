a5e7eb0d59dac9d73ab3e3f34dbe605de2cc9d8a
RenTi <LorenzRenti@outlook.com>
2024-04-04 10:42:07 +0200

    Changed API & added date_last_update to json

--- create_database.py
+++ create_database.py
@@ -1,18 +1,19 @@
 import requests
 import json
 from tqdm import tqdm
+import os
+import time
+from dotenv import load_dotenv
 
-def get_airport_coordinates(icao_code):
-    url = "https://airport-info.p.rapidapi.com/airport"
-
-    querystring = {"icao":icao_code}
+def get_airport_info(icao_code):
+    load_dotenv()
     
-    headers = {
-        "X-RapidAPI-Key": "59de95f2f9msh50bea0dca7488afp1156c6jsnfc81942ea3dc",
-        "X-RapidAPI-Host": "airport-info.p.rapidapi.com"
-    }
+    apiToken = os.getenv('API_TOKEN')
+    ICAO = icao_code
 
-    response = requests.get(url, headers=headers, params=querystring)
+    url = f"https://airportdb.io/api/v1/airport/{ICAO}?apiToken={apiToken}"
+
+    response = requests.get(url)
     
     if response.status_code == 200:
         data = response.json()
@@ -20,12 +21,12 @@ def get_airport_coordinates(icao_code):
             name = data['name']
         else:
             name = None
-        if 'latitude' in data:
-            latitude = data['latitude']
+        if 'latitude_deg' in data:
+            latitude = data['latitude_deg']
         else:
             latitude = None
-        if 'longitude' in data:
-            longitude = data['longitude']
+        if 'longitude_deg' in data:
+            longitude = data['longitude_deg']
         else:
             longitude = None
         return name, latitude, longitude
@@ -34,34 +35,31 @@ def get_Routes(path):
     with open(path, 'r') as f:
         data = json.load(f)
         
-        count = 0
-        for item in data['routes']:
-            count = count + 1
-        
         departure_destination_list = data['routes']
         
         newdata = {
             'name': data['name'],
             'id': data['id'],
+            'date_last_update:': time.strftime('%d-%m-%Y, %H:%M:%S'),
             'routes': [],
         }
         
         for item in tqdm(departure_destination_list):
-            for airport_type in ['departure', 'destination']:
-                if airport_type in item:
-                    icao_code = item[airport_type]
-                    airport_info = get_airport_coordinates(icao_code)
+            for airport in ['departure', 'destination']:
+                if airport in item:
+                    icao_code = item[airport]
+                    airport_info = get_airport_info(icao_code)
                     if airport_info:
                         name, latitude, longitude = airport_info
-                        if latitude:
-                            item[airport_type] = {
+                        if latitude is not None and longitude is not None:
+                            item[airport] = {
                                 'ICAO': icao_code,
                                 'name': name,
                                 'latitude': latitude,
                                 'longitude': longitude
                             }
                         else:
-                            item[airport_type] = {
+                            item[airport] = {
                                 'ICAO': icao_code,
                                 'name': name,
                                 'latitude': None,
