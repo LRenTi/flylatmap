75548a1500c13102525d96bead8a2b579e79b9ca
RenTi <LorenzRenti@outlook.com>
2024-04-09 19:58:50 +0200

    python implemented auto

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 import database as db
 import create_site as cs
 import os
+import json
 
 def main():
     
@@ -10,8 +11,12 @@ def main():
     airport_table = db.Hashtable()
     airport_table.loadTable(filename)
     
-    # Iterate over all airline IDs in the JSON
-    for airline_id in airlinefile:
+    with open('data/airlines.json', 'r') as f:
+        data = json.load(f)
+        print(data)
+    
+    for airlines in data['airlines']:
+        airline_id = airlines["id"]
         path = db.extract_departure_destination(airline_id)
         
         if path:
@@ -19,27 +24,10 @@ def main():
             if os.path.exists(path):
                 os.remove(path)
             #cs.create_site(airline_id)
+            print("Data has been retrieved for airline ID:", airline_id)
         else:
             print("Data could not be retrieved for airline ID:", airline_id)
     
-    
-    print("You find the airline ID in the URL: e.g. https://flylat.net/company/100172")
-    id = input("Enter the airline ID: ")
-    print("Please wait, the data is being retrieved from flylat.net")
-    
-    
-    path = db.extract_departure_destination(id)
-    
-    if path:
-        db.get_Routes(path, airport_table)
-        if os.path.exists(path):
-            os.remove(path)
-        #cs.create_site(id)
-    else:
-        print("Data could not be retrieved from flylat.net")
-        
-    
-
     airport_table.saveTable("airports")
 
 if __name__ == "__main__":
